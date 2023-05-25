489effed092d787c25220c86213b043f7f9e97ba
Thulani Mabaso <Thulanitnm@gmail.com>
2023-05-25 14:27:02 +0200

    allow filtering of data to be done in-memory.

--- Delegate-Training-Registration.DataAccess/Contracts/IRepository.cs
+++ Delegate-Training-Registration.DataAccess/Contracts/IRepository.cs
@@ -1,10 +1,8 @@
-﻿using System.Linq.Expressions;
-
-namespace Delegate_Training_Registration.DataAccess.Contracts
+﻿namespace Delegate_Training_Registration.DataAccess.Contracts
 {
     public interface IRepository<T>
     {
-        IQueryable<T> GetAll(bool isTrackingChanges);
-        IQueryable<T> GetByCondition(Expression<Func<T, bool>> expression, bool isTrackingChanges);
+        IEnumerable<T> GetAll(bool isTrackingChanges);
+        IEnumerable<T> GetByCondition(Func<T, bool> condition, bool isTrackingChanges);
     }
 }
--- Delegate-Training-Registration.DataAccess/Repositories/Repository.cs
+++ Delegate-Training-Registration.DataAccess/Repositories/Repository.cs
@@ -1,31 +1,28 @@
 ﻿using Delegate_Training_Registration.DataAccess.Contracts;
 using Delegate_Training_Registration.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
-using System.Linq.Expressions;
 
 namespace Delegate_Training_Registration.DataAccess.Repositories
 {
     public class Repository<T> : IRepository<T> where T : class
     {
         private readonly DelegateTrainingRegistrationContext _context;
-        private DbSet<T> entity;
+        private DbSet<T> table;
         public Repository(DelegateTrainingRegistrationContext context)
         {
             this._context = context;
-            this.entity = context.Set<T>();
+            this.table = context.Set<T>();
         }
-        public IQueryable<T> GetAll(bool isTrackingChanges)
+
+        public IEnumerable<T> GetAll(bool isTrackingChanges)
         {
-            var resultSet = isTrackingChanges ? this.entity : this.entity.AsNoTracking();
+            var resultSet = isTrackingChanges ? this.table : this.table.AsNoTracking();
             return resultSet;
         }
 
-        public IQueryable<T> GetByCondition(Expression<Func<T, bool>> expression, bool isTrackingChanges)
+        public IEnumerable<T> GetByCondition(Func<T, bool> condition, bool isTrackingChanges)
         {
-            var resultSet = isTrackingChanges ?
-                                    this.entity.Where(expression)
-                                    : this.entity.Where(expression).AsNoTracking();
-
+            var resultSet = isTrackingChanges ? this.table.Where(condition) : this.table.AsNoTracking().Where(condition);
             return resultSet;
         }
     }
