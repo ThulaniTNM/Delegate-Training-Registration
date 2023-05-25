ca7990c7f48fa8db1fa5cbf5e19dd051b7a701b2
Thulani Mabaso <Thulanitnm@gmail.com>
2023-05-25 10:39:06 +0200

    Repository get methods implementation.

---
+++ Delegate-Training-Registration.DataAccess/Contracts/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq.Expressions;
+
+namespace Delegate_Training_Registration.DataAccess.Contracts
+{
+    public interface IRepository<T>
+    {
+        IQueryable<T> GetAll(bool isTrackingChanges);
+        IQueryable<T> GetByCondition(Expression<Func<T, bool>> expression, bool isTrackingChanges);
+    }
+}
---
+++ Delegate-Training-Registration.DataAccess/Repositories/Repository.cs
@@ -0,0 +1,32 @@
+﻿using Delegate_Training_Registration.DataAccess.Contracts;
+using Delegate_Training_Registration.DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Delegate_Training_Registration.DataAccess.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly DelegateTrainingRegistrationContext _context;
+        private DbSet<T> entity;
+        public Repository(DelegateTrainingRegistrationContext context)
+        {
+            this._context = context;
+            this.entity = context.Set<T>();
+        }
+        public IQueryable<T> GetAll(bool isTrackingChanges)
+        {
+            var resultSet = isTrackingChanges ? this.entity : this.entity.AsNoTracking();
+            return resultSet;
+        }
+
+        public IQueryable<T> GetByCondition(Expression<Func<T, bool>> expression, bool isTrackingChanges)
+        {
+            var resultSet = isTrackingChanges ?
+                                    this.entity.Where(expression)
+                                    : this.entity.Where(expression).AsNoTracking();
+
+            return resultSet;
+        }
+    }
+}
