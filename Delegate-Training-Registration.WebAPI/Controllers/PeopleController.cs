6ba0c464209e2e03ff6d6f52a4e8859a807dfbdb
Thulani Mabaso <Thulanitnm@gmail.com>
2023-06-11 19:41:10 +0200

    Add person creation endpoint & call service for registering a person to a course training.

---
+++ Delegate-Training-Registration.WebAPI/Controllers/PeopleController.cs
@@ -0,0 +1,27 @@
+﻿using Delegate_Training_Registration.BusinessServices.Service_Contract;
+using Delegate_Training_Registration.DataAccess.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Delegate_Training_Registration.WebAPI.Controllers
+{
+    [Route("api/people")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        private readonly IPersonService _personService;
+
+        public PeopleController(IPersonService personService)
+        {
+            this._personService = personService;
+        }
+        // can add addtional features of other crud operations if requirements changes.
+        [HttpPost]
+        public ActionResult<Person> CreatePerson([FromBody] Person personFormData)
+        {
+            var person = this._personService.RegisterPerson(personFormData);
+
+            // GetPerson method not existing yet, can be created through product requirement updates.
+            return CreatedAtRoute("GetPerson", new { id = person.PersonID }, person);
+        }
+    }
+}
