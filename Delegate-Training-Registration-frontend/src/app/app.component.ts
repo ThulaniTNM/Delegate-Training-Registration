7eda1db18cb956b38d110bbf603d00e33fd5d1f6
Thulani Mabaso <Thulanitnm@gmail.com>
2023-07-22 15:02:43 +0200

    add navigation toggling & navigation item dropdown toggling.

--- Delegate-Training-Registration-frontend/src/app/app.component.ts
+++ Delegate-Training-Registration-frontend/src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Delegate-Training-Registration-frontend';
+  toggleNav: boolean;
+  toggleNavItemDropdown: boolean;
 }
